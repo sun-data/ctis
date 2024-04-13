73804d6fd6a7943ddfc7f48a914103686c22c8c6
Roy Smart <roytsmart@gmail.com>
2024-04-13 16:18:35 -0600

    Fixing black errors.

--- ctis/instruments/__init__.py
+++ ctis/instruments/__init__.py
@@ -1,9 +1,10 @@
 """
 Models of CTIS instruments used during inversions.
 """
+
 from ._instruments import AbstractInstrument, Instrument
 
 __all__ = [
     "AbstractInstrument",
-    "Instrument"
+    "Instrument",
 ]
